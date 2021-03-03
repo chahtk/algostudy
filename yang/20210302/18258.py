f696a259fab5852b42b310acfb914358c6d001bc
GunYoul Yang <gunyoulyang@GunYoului-MacBookPro.local>
2021-03-03 17:31:10 +0900

    OTL

--- yang/20210302/18258.py
+++ yang/20210302/18258.py
@@ -1,26 +1,26 @@
-a=[]
+# a=[]
 
-n=int(input())
+# n=int(input())
 
-for i in range(n):
-    x=input().split()
+# for i in range(n):
+#     x=input().split()
 
-    if x[0]=='push':
-        a.append(x[1])
-    elif x[0]=='pop':
-        if len(x)==0:
-            return -1
-        else :
-            return -1
-
-
-    elif x[0]=='size':
-
-    elif x[0]=='empyt':
-
-    elif x[0]=='front':
-
-    elif x[0]=='back':
+#     if x[0]=='push':
+#         a.append(x[1])
+#     elif x[0]=='pop':
+#         if len(x)==0:
+#             return -1
+#         else :
+#             return -1
 
 
-print(x[2])
+#     elif x[0]=='size':
+
+#     elif x[0]=='empyt':
+
+#     elif x[0]=='front':
+
+#     elif x[0]=='back':
+
+
+# print(x[2])
