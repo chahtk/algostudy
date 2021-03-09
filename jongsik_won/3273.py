ef3a0666892edcd7f178ece21104df4f6d953034
원종식 <jongsikwon@wonjongsig-ui-MacBookPro.local>
2021-03-09 17:42:25 +0900

    2021.03.09_algorithm

---
+++ jongsik_won/3273.py
@@ -0,0 +1,16 @@
+import sys
+
+# n=int(sys.stdin.readline())
+
+# nums=list(map(int,sys.stdin.readline().split()))
+# ans=0
+# goal=int(sys.stdin.readline())
+# count=[0 for _ in range(2000001)]
+
+# for i in nums:
+#     ans+=count[goal-i]
+#     count[i]+=1
+# print(ans)
+arr=[1,2,3]
+
+print(arr[-1])
